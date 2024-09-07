9af433ad3e6a7bec41d0660c4ee20efa0de2e90f
uit23730225 <23730225@ms.uit.edu.vn>
2024-09-08 06:54:54 +0700

    init function game

---
+++ index.js
@@ -0,0 +1,30 @@
+
+//game loop
+function drawGame() {
+}
+
+function isGameOver() {
+}
+
+function drawScore() {
+}
+
+function clearScreen() {
+}
+
+function drawSnake() {
+}
+
+function changeSnakePosition() {
+}
+
+function drawApple() {
+}
+
+function checkAppleCollision() {
+}
+
+function keyDown(event) {
+}
+
+drawGame()
