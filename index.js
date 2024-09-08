793b833ebda07f96a0d6cb65d6bb9da2ca1403e3
UIT-23730223 <23730223@ms.uit.edu.vn>
2024-09-08 11:57:44 +0700

    add variables & function drawGame()

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");  
+// draw on the screen to get the context, ask canvas  to get the 2d context
+// speed of the game
+let speed = 7;
+// movement
+let inputsXVelocity = 0;
+let inputsYVelocity = 0;
 
+let xVelocity = 0;
+let yVelocity = 0;
 //game loop
 function drawGame() {
-}
+    xVelocity = inputsXVelocity;
+    yVelocity = inputsYVelocity;
+  
+    changeSnakePosition();
+    let result = isGameOver();
+    if (result) {
+      return;
+    }
+  
+    clearScreen();
+  
+    checkAppleCollision();
+    drawApple();
+    drawSnake();
+  
+    drawScore();
+  
+    if (score > 5) {
+      speed = 9;
+    }
+    if (score > 10) {
+      speed = 11;
+    }
+  
+    setTimeout(drawGame, 1000 / speed);
+  }
 
 function isGameOver() {
 }
