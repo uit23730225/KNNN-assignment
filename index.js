894095a758fb8f9879270f2f40bf48d63e4ed861
UIT-23730200 <23730200@ms.uit.edu.vn>
2024-09-08 19:41:46 +0700

    Add function changeSnakePosition

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ function drawSnake() {
 }
 
 function changeSnakePosition() {
+  if (headX === appleX && headY === appleY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+}
 }
 
 function drawApple() {
