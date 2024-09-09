c2b1d0e6bd9fe1a1d5d2735a988bb8cb8dd485a2
DANH-ENVY13\HP <23730160@ms.uit.edu.vn>
2024-09-09 10:58:26 +0700

    GameOver and ChangeSnakePosition

--- code23730160/ChangeSnakePosition.js
+++
@@ -1,8 +0,0 @@
-function changeSnakePosition() {
-  if (headX === appleX && headY === appleY) {
-    appleX = Math.floor(Math.random() * tileCount);
-    appleY = Math.floor(Math.random() * tileCount);
-    tailLength++;
-    score++;
-}
-}
